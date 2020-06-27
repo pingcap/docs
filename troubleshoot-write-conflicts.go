d9c54aa054cbb15fe4fbb3aa2355651eff7049ce
crazycs520 <crazycs520@gmail.com>
2020-06-27 22:25:13 +0800

    update
    Signed-off-by: crazycs520 <crazycs520@gmail.com>

--- troubleshoot-write-conflicts.go
+++ troubleshoot-write-conflicts.go
@@ -48,3 +48,46 @@ You can check the following monitoring metrics under **KV Errors** in the **TiDB
      ![kv-retry-duration](/media/troubleshooting-write-conflict-kv-retry-duration.png)
 
 You can also use `[kv:9007]Write conflict` as the key word to search in the TiDB log. The key word also indicates the write conflict exists in the cluster.
+
+## How to resolve the write-conflict?
+
+If there were many write conflict in the cluster, you can find out the write conflict key and the reason, then try to change the application logic to avoid write conflict. When the write conflict exists, you can see the similar log in TiDB log file:
+
+```log
+[2020/05/12 15:17:01.568 +08:00] [WARN] [session.go:446] ["commit failed"] [conn=3] ["finished txn"="Txn{state=invalid}"] [error="[kv:9007]Write conflict, txnStartTS=416617006551793665, conflictStartTS=416617018650001409, conflictCommitTS=416617023093080065, key={tableID=47, indexID=1, indexValues={string, }} primary={tableID=47, indexID=1, indexValues={string, }} [try again later]"]
+```
+
+The explaination of upper log as below:
+
+* `[kv:9007]Write conflict`: indicates the write conflict.
+* `txnStartTS=416617006551793665`：indicates the `start_ts` of the current transaction. You can use `pd-ctl` tool to convert the `start_ts` to physical time.
+* `conflictStartTS=416617018650001409`: indicates the `start_ts` of the write conflict transaction.
+* `conflictCommitTS=416617023093080065`: indicates the `commit_ts` of the write conflict transaction.
+* `key={tableID=47, indexID=1, indexValues={string, }}`：indicates the write conflict key. `tableID` indicates the ID of the write conflict table. `indexID` indicates the ID of write conflict index. If the write conflict key is a record key, the log will print `handle=x`, indicates which record(row) was conflict. `indexValues` indicates the conflict index value.
+* `primary={tableID=47, indexID=1, indexValues={string, }}`: indicates the primary key information of the current transaction.
+
+You can use `pd-ctl` tool to convert the ts, such as below:
+
+{{< copyable "" >}}
+
+```shell
+./pd-ctl -u https://127.0.0.1:2379 tso {TIMESTAMP}
+```
+
+You can use `tableID` to look up the related table:
+
+{{< copyable "" >}}
+
+```shell
+curl http://{TiDBIP}:10080/db-table/{tableID}
+```
+
+You can use `indexID` and table name to look up the related index:
+
+{{< copyable "sql" >}}
+
+```sql
+SELECT * FROM INFORMATION_SCHEMA.TIDB_INDEXES WHERE TABLE_SCHEMA='{table_name}' AND TABLE_NAME='{table_name}' AND INDEX_ID={indexID};
+```
+
+In addition, in TiDB v3.0.8 and later versions, the defalut transaction model was changed to pessimistic transaction. Pessimistic transaction model can avoid write conflict during the transaction prewrite stage, so no need to modify the application. In the pessimistic transaction mode, each DML statement will write a pessimistic lock to the related keys during execution. The pessimistic lock is used to prevent other transactions from modifying the same key, thus ensuring that there will be no writing conflict in the `prewrite` stage of the transaction 2PC.
