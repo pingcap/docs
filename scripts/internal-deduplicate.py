6794e32733c2ec3eb2bf5a8535d41e2402580365
Grace Cai <qqzczy@126.com>
2022-05-13 16:34:35 +0800

    Update internal-deduplicate.py

--- scripts/internal-deduplicate.py
+++ scripts/internal-deduplicate.py
@@ -15,7 +15,7 @@ def get_note_info(path):
 
 
 if __name__ == "__main__":
-    path = r'/Users/grcai/Documents/GitHub/qiancai/docs/releases/release-5.4.1.md'
+    path = r'D:\GitHub\upstream\docs1\releases\release-5.1.4.md'
     exst_note = get_note_info(path)
     mylist = exst_note
     myset = set(mylist)
